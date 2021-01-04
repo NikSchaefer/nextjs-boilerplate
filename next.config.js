aef82783c25539d23c5b1cf3895ef25c70496641
Nik Schaefer <nikkschaefer@gmail.com>
2021-01-04 14:39:07 -0600

    Clean Up

---
+++ next.config.js
@@ -0,0 +1,3 @@
+moduel.exports = {
+    
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <Head>
+        <title>New App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+
+      </header>
+
+      <Component {...pageProps} />
+      <footer>
+
+      </footer>
+
+    </div>
+  )
 }
-
-export default MyApp
