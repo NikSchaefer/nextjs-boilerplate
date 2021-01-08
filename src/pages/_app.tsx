d880fd7044b92e353e5efa54ae645b6308eaaee9
Nik Schaefer <nikkschaefer@gmail.com>
2021-01-08 11:37:50 -0600

    Mock API

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import "../styles/globals.css";
+import type { AppProps } from "next/app";
 import Head from "next/head";
 
 import Footer from "../components/footer";
 import Header from "../components/header";
 
 // eslint-disable-next-line import/no-default-export
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 	return (
 		<div>
 			<Head>
