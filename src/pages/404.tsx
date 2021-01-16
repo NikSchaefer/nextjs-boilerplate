e0fe5087951789e070c29f753f064703adfd5d4c
Nik Schaefer <nikkschaefer@gmail.com>
2021-01-16 12:29:46 -0600

    MDX Removal

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,11 @@
 import Link from "next/link";
-
+import { useEffect } from 'react'
 // eslint-disable-next-line import/no-default-export
-export default function FourOhFour(): JSX.Element {
+export default function FourOhFour(): JSX.Element
+{
+	useEffect(() => { // redirects user to home
+		window.location.href = '/'
+	}, []);
 	return (
 		<>
 			<h1>404 Page not Found</h1>
