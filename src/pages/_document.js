16d1f86a6fde3f2e6f800587fcaea4bf6ab9f7fe
Nik Schaefer <nikkschaefer@gmail.com>
2021-01-27 15:02:36 -0600

    Meta

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -30,9 +30,50 @@ export default class MyDocument extends Document {
 	}
 	render() {
 		return (
-			<Html>
+			<Html lang='en-us'>
 				<Head>
-					{/* Global Site Tag (gtag.js) - Google Analytics */}
+					<title>NextJS Boilerplate | Nik Schaefer</title>
+					<meta
+						name="description"
+						content="Boilerplate built to scale containing Typescript + NextJS + Google Analytics + ESLint + Jest + Styled Components + Icons"
+					/>
+
+					<meta
+						itemProp="name"
+						content="NextJS Boilerplate | Nik Schaefer"
+					/>
+					<meta
+						itemProp="description"
+						content="Boilerplate built to scale containing Typescript + NextJS + Google Analytics + ESLint + Jest + Styled Components + Icons"
+					/>
+					<meta itemProp="image" content="/logo.png" />
+
+					<meta
+						property="og:url"
+						content="https://boilerplate.nikschaefer.tech"
+					/>
+					<meta property="og:type" content="website" />
+					<meta
+						property="og:title"
+						content="NextJS Boilerplate | Nik Schaefer"
+					/>
+					<meta
+						property="og:description"
+						content="Boilerplate built to scale containing Typescript + NextJS + Google Analytics + ESLint + Jest + Styled Components + Icons"
+					/>
+					<meta property="og:image" content="/logo.png" />
+
+					<meta name="twitter:card" content="summary_large_image" />
+					<meta
+						name="twitter:title"
+						content="NextJS Boilerplate | Nik Schaefer"
+					/>
+					<meta
+						name="twitter:description"
+						content="Boilerplate built to scale containing Typescript + NextJS + Google Analytics + ESLint + Jest + Styled Components + Icons"
+					/>
+					<meta name="twitter:image" content="/logo.png" />
+
 					<script
 						async
 						src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
