fe9cedec0818499a63f36c37b4a31bdb8b883def
Nik Schaefer <nikkschaefer@gmail.com>
2021-01-27 14:26:40 -0600

    Boilerplate

--- src/components/icons/index.tsx
+++
@@ -1,1 +0,0 @@
-export { default as IconGithub } from "./github";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,70 +1,113 @@
-import { IconGithub } from "@components/icons";
+import { GiFeather } from "react-icons/gi";
+import {
+	SiTypescript,
+	SiGoogleanalytics,
+	SiEslint,
+	SiJest,
+	SiStyleshare,
+} from "react-icons/si";
+import { FaReact } from 'react-icons/fa'
 import styled from "styled-components";
+
+import { NextJS } from "../components/icons";
 
 const Container = styled.main`
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
-	height: 100vh;
+	min-height: 600px;
 `;
-const StyledDiv = styled.div`
+const FlexContainer = styled.div`
 	display: flex;
 	flex-wrap: wrap;
-	max-width: 700px;
+	max-width: 1300px;
 	justify-content: center;
 `;
-
-const StyledComponent = styled.div`
-	width: 300px;
+const Box = styled.div`
+	max-width: 370px;
+	width: 100%;
 	text-align: left;
 	box-sizing: border-box;
-	padding: 20px;
+	padding: 40px;
 	margin: 20px;
-	transition: box-shadow 0.3s ease-in-out;
-	box-shadow: 0 0 10px rgba(0, 0, 0, 0.342);
-	& h2 {
-		color: #008cff;
-	}
+	position: relative;
+	background-color: #2e3748;
+	border-radius: 10px;
+	top: 0;
+	transition: all ease-in-out 0.2s;
 	&:hover {
-		box-shadow: 0 0 20px rgba(0, 0, 0, 0.342);
+		top: -8px;
+	}
+	& h2 {
+		color: #52baff;
 	}
 `;
 // eslint-disable-next-line import/no-default-export
 export default function Home(): JSX.Element {
 	return (
 		<Container>
-			<h1>Hello, World</h1>
-			<StyledDiv>
-				<StyledComponent>
-					<h2>This content</h2>
-					<p>You can remove these components in src/pages/index</p>
-				</StyledComponent>
-				<StyledComponent>
+			<Container>
+				<h1>Hello, World</h1>
+			</Container>
+			<h2>Stack</h2>
+			<FlexContainer>
+				<Box>
+					<SiTypescript size="40px" color="#007acd" />
+					<h2>Typescript</h2>
+					<p>
+						Built to ensure long-term usabilty and a cleaner
+						codebase, We use 100% Typescript
+					</p>
+				</Box>
+				<Box>
+					<NextJS />
 					<h2>NextJS</h2>
 					<p>
 						This template uses NextJS, Jest, Eslint-Config-Galex,
 						Google Analytics
 					</p>
-				</StyledComponent>
-				<StyledComponent>
-					<h2>Learn more</h2>
-					<p>Learn more about this boilerplate in the README.md</p>
-				</StyledComponent>
-				<StyledComponent>
-					<h2>Config</h2>
+				</Box>
+				<Box>
+					<SiStyleshare size="40px" />
+					<h2>Styled Components</h2>
+					<p>Using Styled Components to keep a modular code style</p>
+				</Box>
+				<Box>
+					<GiFeather size="40px" color="white" />
+					<h2>Lightweight</h2>
 					<p>
-						You can enter a google analytics measurement id in
-						src/config.ts
+						Setting the baseline configurations to provide an
+						accelerated development enviroment
 					</p>
-				</StyledComponent>
-			</StyledDiv>
-			<a
-				aria-label="Github"
-				href="https://github.com/NikSchaefer/nextjs-typescript"
-			>
-				<IconGithub />
-			</a>
+				</Box>
+				<Box>
+					<SiGoogleanalytics size="40px" color="#f0ae33" />
+					<h2>Google Analytics</h2>
+					<p>
+						You can set a tracking Id in src/config.ts to track
+						analytics
+					</p>
+				</Box>
+				<Box>
+					<SiEslint size="40px" color="purple" />
+					<h2>ESLint</h2>
+					<p>
+						Using a package of 30+ Eslint Plugins to ensure clean
+						code quality and acessibility
+					</p>
+				</Box>
+				<Box>
+					<SiJest size="40px" color="red" />
+					<h2>Jest</h2>
+					<p>All tests, integration and unit run throught jest</p>
+				</Box>
+				<Box>
+					<FaReact size="40px" color="green" />
+					<h2>React Icons</h2>
+					<p>Icons from popular packages. <a style={{color:'#00ffaa'}} href="https://react-icons.github.io/react-icons">All Icons</a> </p>
+				</Box>
+			</FlexContainer>
 		</Container>
 	);
 }
