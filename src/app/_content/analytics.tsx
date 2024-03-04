608d4ddbd7e0fb18c9aef4a683060ee563d15bc3
Hazim Arafa <hazimarafa80@gmail.com>
2024-03-04 09:59:21 -0800

    show one line so in dev tools no new line

--- src/app/_content/analytics.tsx
+++ src/app/_content/analytics.tsx
@@ -20,14 +20,7 @@ export default function AnalyticScripts() {
 		<Script
 			strategy="afterInteractive"
 			dangerouslySetInnerHTML={{
-				__html: `
-!function(a,b){var c=window;c.SessionStackKey=a,c[a]=c[a]||{t:b,
-q:[]};for(var d=["start","stop","identify","getSessionId","log","setOnDataCallback","trackEvent"],e=0;e<d.length;e++)!function(b){
-c[a][b]=c[a][b]||function(){c[a].q.push([b].concat([].slice.call(arguments,0)));
-}}(d[e]);var f=document.createElement("script");f.async=1,f.crossOrigin="anonymous",
-f.src="https://cdn.sessionstack.com/sessionstack.js";var g=document.getElementsByTagName("script")[0];
-g.parentNode.insertBefore(f,g)}("SessionStack","abed4f88c14446ef88144ed350195b15");
-`,
+				__html: `!function(a,b){var c=window;c.SessionStackKey=a,c[a]=c[a]||{t:b,q:[]};for(var d=["start","stop","identify","getSessionId","log","setOnDataCallback","trackEvent"],e=0;e<d.length;e++)!function(b){c[a][b]=c[a][b]||function(){c[a].q.push([b].concat([].slice.call(arguments,0)));}}(d[e]);var f=document.createElement("script");f.async=1,f.crossOrigin="anonymous",f.src="https://cdn.sessionstack.com/sessionstack.js";var g=document.getElementsByTagName("script")[0];g.parentNode.insertBefore(f,g)}("SessionStack","abed4f88c14446ef88144ed350195b15");`,
 			}}
 		/>
 	);
