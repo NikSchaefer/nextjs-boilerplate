505a95edfa3e90d353ede522f7cf75c300e3fe97
Hazim Arafa <hazimarafa80@gmail.com>
2024-04-24 22:04:51 -0700

    prettier on all ui files

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import * as React from "react";
-import useEmblaCarousel from "embla-carousel-react";
 import {
 	type EmblaCarouselType as CarouselApi,
 	type EmblaOptionsType as CarouselOptions,
 	type EmblaPluginType as CarouselPlugin,
 } from "embla-carousel";
+import useEmblaCarousel from "embla-carousel-react";
 import { ArrowLeft, ArrowRight } from "lucide-react";
+import * as React from "react";
 
-import { cn } from "lib/utils";
 import { Button } from "components/ui/button";
+import { cn } from "lib/utils";
 
 type CarouselProps = {
 	opts?: CarouselOptions;
@@ -252,10 +252,10 @@ const CarouselNext = React.forwardRef<
 CarouselNext.displayName = "CarouselNext";
 
 export {
-	type CarouselApi,
 	Carousel,
 	CarouselContent,
 	CarouselItem,
-	CarouselPrevious,
 	CarouselNext,
+	CarouselPrevious,
+	type CarouselApi,
 };
